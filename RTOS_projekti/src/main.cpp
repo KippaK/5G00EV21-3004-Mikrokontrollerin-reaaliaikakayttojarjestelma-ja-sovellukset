2ff9305a05304c2992e52cd495d2ec870f8d1926
Antti Venetjoki <antti.venetjoki@gmail.com>
2025-04-04 12:03:16 +0300

    Progress

--- RTOS_projekti/src/main.cpp
+++ RTOS_projekti/src/main.cpp
@@ -1,31 +1,32 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
 
 #define KEYPAD_DEBOUNCE_MS 50
+#define EXT0_WAKEUP_HIGH 1
 
-#define KEYPAD_NOT_PRESSED -1
-#define KEYPAD_0 0
-#define KEYPAD_1 1
-#define KEYPAD_2 2
-#define KEYPAD_3 3
-#define KEYPAD_4 4
-#define KEYPAD_5 5
-#define KEYPAD_6 6
-#define KEYPAD_7 7
-#define KEYPAD_8 8
-#define KEYPAD_9 9
-#define KEYPAD_A 10
-#define KEYPAD_B 11
-#define KEYPAD_C 12
-#define KEYPAD_D 13
-#define KEYPAD_STAR 14
-#define KEYPAD_HASH 15
+#define KEYPAD_WAKEUP_INPUT_PIN GPIO_NUM_39
+#define KEYPAD_WAKEUP_OUTPUT_PIN GPIO_NUM_26
 
-const int KEYPAD[4][4] = {
-  {KEYPAD_1, KEYPAD_2, KEYPAD_3, KEYPAD_A},
-  {KEYPAD_4, KEYPAD_5, KEYPAD_6, KEYPAD_B},
-  {KEYPAD_7, KEYPAD_8, KEYPAD_9, KEYPAD_C},
-  {KEYPAD_STAR, KEYPAD_0, KEYPAD_HASH, KEYPAD_D}
+const char *WIFI_SSID = "SOURCE";
+const char *WIFI_PASSWORD = "Pelle!23";
+
+AsyncWebServer server(80);
+
+QueueHandle_t keypadQueue;
+
+TimerHandle_t inactivityTimer;
+
+touch_pad_t touchPin;
+
+const char KEYPAD_NOT_PRESSED = 0;
+
+const char KEYPAD[4][4] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
 
 const int KEYPAD_GPIO_IN[4] = {GPIO_NUM_36, GPIO_NUM_37, GPIO_NUM_38, GPIO_NUM_39};
@@ -34,7 +35,11 @@ const int KEYPAD_GPIO_OUT[4] = {GPIO_NUM_26, GPIO_NUM_25, GPIO_NUM_33, GPIO_NUM_
 
 TFT_eSPI tft = TFT_eSPI();
 
-int checkKeypadStatus() {
+void IRAM_ATTR keypadISR() {
+
+}
+
+char checkKeypadStatus() {
     for (int i = 0; i < 4; i++) {
         digitalWrite(KEYPAD_GPIO_OUT[i], HIGH);
         delay(1);
@@ -52,14 +57,29 @@ int checkKeypadStatus() {
     return KEYPAD_NOT_PRESSED;
 }
 
-int waitForKeyPress() {
-  int keypadStatus = KEYPAD_NOT_PRESSED;
+char waitForKeyPress() {
+  char keypadStatus = KEYPAD_NOT_PRESSED;
   for (;;) {
     keypadStatus = checkKeypadStatus();
     if (keypadStatus != KEYPAD_NOT_PRESSED) {
       return keypadStatus;
     }
   }
+}
+
+void setupApiEndpoints() {
+  server.on("/api/open", HTTP_GET, [](AsyncWebServerRequest *request){
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(0, 0);
+    tft.print("Open");
+    request->send(200, "application/json");
+  });
+  server.on("/api/close", HTTP_GET, [](AsyncWebServerRequest *request){
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(0, 0);
+    tft.print("close");
+    request->send(200, "application/json");
+  });
 }
 
 void setup() {
@@ -73,19 +93,36 @@ void setup() {
   }
   
   Serial.begin(115200);
+  
   tft.init();
   tft.begin();
-  tft.setRotation(1); 
+  tft.setRotation(3); 
   tft.setTextDatum(MC_DATUM);
   tft.setTextFont(2);
   tft.setTextColor(TFT_WHITE);
   tft.setTextSize(2);
+  tft.fillScreen(TFT_BLACK);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  tft.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    tft.print('.');
+    delay(500);
+  }
+  tft.setCursor(0, 0);
+  tft.fillScreen(TFT_BLACK);
+  tft.println("Connected to WiFi network");
+  tft.print(WiFi.localIP());
+
+  setupApiEndpoints();
+
+  server.begin();
 }
 
 void loop() {
-  int keyPress = waitForKeyPress();
+  char keyPress = waitForKeyPress();
   Serial.println(keyPress);
-  tft.fillScreen(TFT_WHITE);
+  tft.fillScreen(TFT_BLACK);
   tft.setCursor(50, 50);
   tft.println(keyPress);
 }
