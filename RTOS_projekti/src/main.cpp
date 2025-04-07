e34415cd198a585d9dcc8e31f7b2d94343419d65
Antti Venetjoki <antti.venetjoki@gmail.com>
2025-04-07 15:44:36 +0300

    Progress

--- RTOS_projekti/src/main.cpp
+++ RTOS_projekti/src/main.cpp
@@ -2,6 +2,9 @@
 #include <TFT_eSPI.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+#include <FS.h>
+#include <vector>
 
 #define KEYPAD_DEBOUNCE_MS 50
 #define EXT0_WAKEUP_HIGH 1
@@ -9,19 +12,32 @@
 #define KEYPAD_WAKEUP_INPUT_PIN GPIO_NUM_39
 #define KEYPAD_WAKEUP_OUTPUT_PIN GPIO_NUM_26
 
+#define DOOR_OPEN 1
+#define DOOR_CLOSED 0
+
+#define MINUTE_MS 60000
+
 const char *WIFI_SSID = "SOURCE";
 const char *WIFI_PASSWORD = "Pelle!23";
 
+const String PASSCODE = "8437";
+
+bool users_updated = false;
+
+int8_t door_status = DOOR_CLOSED;
+
 AsyncWebServer server(80);
-
 QueueHandle_t keypadQueue;
-
 TimerHandle_t inactivityTimer;
-
 touch_pad_t touchPin;
 
-const char KEYPAD_NOT_PRESSED = 0;
+TaskHandle_t *db_update_task_h = new TaskHandle_t;
+SemaphoreHandle_t xSemaphore = NULL;
 
+fs::File db_file;
+std::vector<String> db_vec;
+
+const char KEYPAD_NOT_PRESSED = 0;
 const char KEYPAD[4][4] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
@@ -67,22 +83,147 @@ char waitForKeyPress() {
   }
 }
 
+bool authenticateUser(AsyncWebServerRequest *request) {
+  if (!request->hasHeader("username")) { return false; }
+  if (!request->hasHeader("password")) { return false; }
+  String user = request->getHeader("username")->value();
+  user += ", ";
+  user += request->getHeader("password")->value();
+  for (int i = 0; i < db_vec.size(); i++) {
+    if (db_vec.at(i) == user) {
+      return true;
+    }
+  }
+  return false;
+}
+
 void setupApiEndpoints() {
-  server.on("/api/open", HTTP_GET, [](AsyncWebServerRequest *request){
-    tft.fillScreen(TFT_BLACK);
-    tft.setCursor(0, 0);
-    tft.print("Open");
-    request->send(200, "application/json");
+  // Door Status GET Endpoint
+  server.on("/api/status", HTTP_GET, [](AsyncWebServerRequest * request){
+    String door_status_str;
+    if (door_status == DOOR_OPEN) { door_status_str = "open"; }
+    if (door_status == DOOR_CLOSED) { door_status_str = "closed"; }
+    String message = "{\"message\": \"Door is " + door_status_str +"\", \"door_status\": \""+ String(door_status) + "\"}";
+    request->send(200, "application/json", message);
   });
-  server.on("/api/close", HTTP_GET, [](AsyncWebServerRequest *request){
-    tft.fillScreen(TFT_BLACK);
-    tft.setCursor(0, 0);
-    tft.print("close");
-    request->send(200, "application/json");
+
+  // Open Door POST endpoint
+  server.on("/api/open", HTTP_POST, [](AsyncWebServerRequest *request){
+    if (authenticateUser(request)) {
+      door_status = DOOR_OPEN;
+      tft.fillScreen(TFT_BLACK);
+      tft.setCursor(0, 0);
+      tft.print("Open");
+      request->send(200, "application/json");
+    }
+    else {
+      request->send(401, "application/json");
+    }
+  });
+
+  // Close Door POST endpoint
+  server.on("/api/close", HTTP_POST, [](AsyncWebServerRequest *request){
+    if (authenticateUser(request)) {
+      door_status = DOOR_CLOSED;
+      tft.fillScreen(TFT_BLACK);
+      tft.setCursor(0, 0);
+      tft.print("Close");
+      request->send(200, "application/json");
+    }
+    else {
+      request->send(401, "application/json");
+    }
+  });
+
+  // Add User POST endpoint
+  server.on("/api/adduser", HTTP_POST, [](AsyncWebServerRequest *request){
+    if (!request->hasHeader("passcode") || !request->hasHeader("password") || !request->hasHeader("username")) {
+      request->send(400, "application/json", "{\"error\": \"Missing headers\"}");
+      return;
+    }
+    String received_passcode = request->getHeader("passcode")->value();
+    if (received_passcode != PASSCODE) {
+      request->send(401, "application/json", "{\"error\": \"Invalid passcode\"}");
+      return;
+    }
+    String username = request->getHeader("username")->value();
+    String password = request->getHeader("password")->value();
+    String new_user = username + ", " + password;
+    if (xSemaphoreTake(xSemaphore, pdMS_TO_TICKS(5000)) == pdTRUE) {
+      db_vec.push_back(new_user);
+      users_updated = true;
+      request->send(200, "application/json", "{\"message\": \"User added successfully\"}");
+      xSemaphoreGive(xSemaphore);
+      return;
+    }
+    else {
+      request->send(500, "application/json", "{\"error\": \"Internal server error during user creation\"}");
+      return;
+    }
   });
 }
 
+void db_init() {
+  if (!SPIFFS.begin(false)) {
+    Serial.println("SPIFFS Mount Failed");
+    return;
+  } else {
+    Serial.println("SPIFFS Mounted Successfully");
+  }
+
+  db_file = SPIFFS.open("/db.csv", "r+");
+  if (!db_file) {
+    db_file = SPIFFS.open("/db.csv", FILE_WRITE);
+    db_file.println("admin, admin");
+    db_vec.push_back("admin, admin");
+  }
+  if (!db_file) {
+    Serial.println("Failed to open or create db file");
+    return;
+  }
+  while (db_file.available()) {
+    db_vec.push_back(db_file.readStringUntil('\n'));
+  }
+  db_file.close();
+}
+
+void update_db_file_task(void *params) {
+  for (;;) {
+    if (users_updated) {
+      if (db_file) {
+        db_file.close();
+      }
+      db_file = SPIFFS.open("/db.csv", FILE_WRITE);
+      for (int i = 0; i < db_vec.size(); i++) {
+        db_file.println(db_vec.at(i));
+      }
+    Serial.println("Updated database file from task");
+    users_updated = false;
+    }
+    vTaskDelay(MINUTE_MS * 5 / portTICK_PERIOD_MS);
+  }
+}
+
+void task_init() {
+  xTaskCreate(
+    update_db_file_task,
+    "Update database file",
+    2048,
+    NULL,
+    1,
+    db_update_task_h
+  );
+}
+
 void setup() {
+  Serial.begin(115200);
+  db_init();
+  xSemaphore = xSemaphoreCreateMutex();
+  if(xSemaphore == NULL) {
+    Serial.println("Failed to create semaphore");
+    return;
+  }
+  task_init();
   // port init for servo
 
 
@@ -92,8 +233,8 @@ void setup() {
     pinMode(KEYPAD_GPIO_OUT[i], OUTPUT);
   }
   
-  Serial.begin(115200);
   
+
   tft.init();
   tft.begin();
   tft.setRotation(3); 
@@ -104,7 +245,7 @@ void setup() {
   tft.fillScreen(TFT_BLACK);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  tft.println("Connecting to WiFi");
+  tft.println("Connecting to wifi");
   while (WiFi.status() != WL_CONNECTED) {
     tft.print('.');
     delay(500);
