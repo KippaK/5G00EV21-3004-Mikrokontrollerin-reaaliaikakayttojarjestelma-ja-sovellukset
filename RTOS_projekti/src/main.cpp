0ae471c6221f65825e45c87bfa78e3c0e5ffad32
Antti Venetjoki <antti.venetjoki@gmail.com>
2025-04-28 00:44:45 +0300

    Possibly the final version, hasn't been tested

--- RTOS_projekti/src/main.cpp
+++ RTOS_projekti/src/main.cpp
@@ -5,6 +5,9 @@
 #include <SPIFFS.h>
 #include <FS.h>
 #include <vector>
+#include <ESP32Servo.h>
+#include "lock.h"
+#include "unlock.h"
 
 #define KEYPAD_DEBOUNCE_MS 50
 #define EXT0_WAKEUP_HIGH 1
@@ -12,19 +15,14 @@
 #define KEYPAD_WAKEUP_INPUT_PIN GPIO_NUM_39
 #define KEYPAD_WAKEUP_OUTPUT_PIN GPIO_NUM_26
 
+#define KEYPAD_NOT_PRESSED -1
 #define DOOR_OPEN 1
 #define DOOR_CLOSED 0
 
 #define MINUTE_MS 60000
 
-#define SOURCE_VERKKO 0
-#if SOURCE_VERKKO
 const char *WIFI_SSID = "SOURCE";
 const char *WIFI_PASSWORD = "Pelle!23";
-#else
-const char *WIFI_SSID = "Kekoaly";
-const char *WIFI_PASSWORD = "aivosolu";
-#endif
 
 const String PASSCODE = "8437";
 
@@ -33,17 +31,39 @@ bool users_updated = false;
 int8_t door_status = DOOR_CLOSED;
 
 AsyncWebServer server(80);
+
+QueueHandle_t displayQueue;
 QueueHandle_t keypadQueue;
+QueueHandle_t codeInputQueue;
+
 TimerHandle_t inactivityTimer;
+
 touch_pad_t touchPin;
 
+TaskHandle_t *displayTaskHandle = new TaskHandle_t;
+TaskHandle_t *codeTaskHandle = new TaskHandle_t;
 TaskHandle_t *db_update_task_h = new TaskHandle_t;
 SemaphoreHandle_t xSemaphore = NULL;
 
 fs::File db_file;
 std::vector<String> db_vec;
 
-const char KEYPAD_NOT_PRESSED = 0;
+// === TFT Setup ===
+TFT_eSPI tft = TFT_eSPI();
+TFT_eSprite backgroundSprite = TFT_eSprite(&tft);
+TFT_eSprite unlockSprite = TFT_eSprite(&tft);
+TFT_eSprite lockSprite = TFT_eSprite(&tft);
+
+// === Servo & Buzzer ===
+Servo myServo;
+const int servoPin = 17 ;
+const int buzzerPin = 13;
+
+// === Password Setup ===
+const char passwordCode[4] = {'1', '2', '3', '4'};
+const int expectedLength = 4;
+const int positions[4] = {42, 90, 138, 186};
+
 const char KEYPAD[4][4] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
@@ -55,39 +75,19 @@ const int KEYPAD_GPIO_IN[4] = {GPIO_NUM_36, GPIO_NUM_37, GPIO_NUM_38, GPIO_NUM_3
 
 const int KEYPAD_GPIO_OUT[4] = {GPIO_NUM_26, GPIO_NUM_25, GPIO_NUM_33, GPIO_NUM_32};
 
-TFT_eSPI tft = TFT_eSPI();
+enum DisplayState {
+  DISPLAY_IDLE,
+  DISPLAY_ENTER_CODE,
+  DISPLAY_STAR,
+  DISPLAY_UNLOCK,
+  DISPLAY_WRONG
+};
 
-void IRAM_ATTR keypadISR() {
+typedef struct {
+  DisplayState state;
+  int position;
+} DisplayMessage;
 
-}
-
-char checkKeypadStatus() {
-    for (int i = 0; i < 4; i++) {
-        digitalWrite(KEYPAD_GPIO_OUT[i], HIGH);
-        delay(1);
-        for (int j = 0; j < 4; j++) {
-            if (digitalRead(KEYPAD_GPIO_IN[j]) == HIGH) {
-                delay(KEYPAD_DEBOUNCE_MS);
-                while (digitalRead(KEYPAD_GPIO_IN[j]) == HIGH);
-                delay(KEYPAD_DEBOUNCE_MS);
-                digitalWrite(KEYPAD_GPIO_OUT[i], LOW);
-                return KEYPAD[j][i];
-            }
-        }
-        digitalWrite(KEYPAD_GPIO_OUT[i], LOW);
-    }
-    return KEYPAD_NOT_PRESSED;
-}
-
-char waitForKeyPress() {
-  char keypadStatus = KEYPAD_NOT_PRESSED;
-  for (;;) {
-    keypadStatus = checkKeypadStatus();
-    if (keypadStatus != KEYPAD_NOT_PRESSED) {
-      return keypadStatus;
-    }
-  }
-}
 
 void addCORS(AsyncWebServerResponse *response) {
   response->addHeader("Access-Control-Allow-Origin", "*");
@@ -236,15 +236,228 @@ void update_db_file_task(void *params) {
   }
 }
 
-void task_init() {
-  xTaskCreate(
-    update_db_file_task,
-    "Update database file",
-    2048,
-    NULL,
-    1,
-    db_update_task_h
-  );
+
+void displayTask(void *pvParameters) {
+  DisplayMessage msg;
+
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(3);
+
+  while (true) {
+    if (xQueueReceive(displayQueue, &msg, portMAX_DELAY)) {
+      switch (msg.state) {
+        case DISPLAY_IDLE:
+          tft.fillScreen(TFT_WHITE);
+          tft.setTextColor(TFT_BLACK);
+          tft.setTextSize(3);
+          tft.drawString("Welcome", 60, 20);  // Or any idle UI
+          break;
+        case DISPLAY_ENTER_CODE:
+          tft.fillScreen(TFT_WHITE);
+          tft.drawString("Enter Code", 30, 30);
+          break;
+        case DISPLAY_STAR:
+          tft.setTextColor(TFT_BLACK, TFT_WHITE);
+          tft.drawString("*", msg.position, 84);
+          break;
+        case DISPLAY_UNLOCK:
+          backgroundSprite.fillSprite(TFT_WHITE);
+          unlockSprite.pushToSprite(&backgroundSprite, 88, 50, TFT_WHITE);
+          backgroundSprite.pushSprite(0, 0);
+          tft.drawString("Open!", 80, 20);
+          break;
+        case DISPLAY_WRONG:
+          backgroundSprite.fillSprite(TFT_WHITE);
+          lockSprite.pushToSprite(&backgroundSprite, 88, 50, TFT_WHITE);
+          backgroundSprite.pushSprite(0, 0);
+          tft.drawString("Wrong", 80, 20);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
+
+void driveServo(){
+  myServo.attach(servoPin);
+  switch(door_status){
+    case DOOR_OPEN:
+      myServo.write(90);
+      Serial.println("[driveServo] Door Opened");
+      vTaskDelay(1500 / portTICK_PERIOD_MS);
+      break;
+    case DOOR_CLOSED:
+      myServo.write(0);
+      Serial.println("[driveServo] Door Closed");
+      vTaskDelay(1500 / portTICK_PERIOD_MS);
+      break;
+    default:
+      myServo.write(0);
+      break;
+  }
+  myServo.detach();
+}
+
+void openDoor(){
+  door_status = DOOR_OPEN;
+  driveServo();
+}
+void closeDoor(){
+  door_status = DOOR_CLOSED;
+  driveServo();
+}
+void getCodeTask(void *pvParameters) {
+  Serial.println("Code Task Started");
+  int key;
+  int inputCount = 0;
+  int correctCount = 0;
+
+  DisplayMessage msg;
+
+  // Clear screen before starting
+  msg.state = DISPLAY_ENTER_CODE;
+  xQueueSend(displayQueue, &msg, portMAX_DELAY);
+
+  while (true) {
+    if (xQueueReceive(codeInputQueue, &key, portMAX_DELAY)) {
+      if (key == '#') {
+        break;
+      }
+      else if (key == '*') {
+        // Reset input on '*'
+        inputCount = 0;
+        correctCount = 0;
+
+        msg.state = DISPLAY_ENTER_CODE;
+        xQueueSend(displayQueue, &msg, portMAX_DELAY);
+        continue;
+      }
+
+      // Only process keys if within input range
+      if (inputCount < expectedLength) {
+        msg.state = DISPLAY_STAR;
+
+        // Avoid accessing out-of-bounds position
+        if (inputCount < sizeof(positions) / sizeof(positions[0])) {
+          msg.position = positions[inputCount];
+        } else {
+          msg.position = 0; // fallback to safe position
+        }
+
+        xQueueSend(displayQueue, &msg, portMAX_DELAY);
+
+        if (key == passwordCode[inputCount]) {
+          correctCount++;
+        }
+
+        inputCount++;
+      }
+    }
+  }
+
+  if (correctCount == expectedLength) {
+    msg.state = DISPLAY_UNLOCK;
+    xQueueSend(displayQueue, &msg, portMAX_DELAY);
+    openDoor();
+    tone(buzzerPin, 75);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    noTone(buzzerPin);
+  } else {
+    msg.state = DISPLAY_WRONG;
+    xQueueSend(displayQueue, &msg, portMAX_DELAY);
+    for (int i = 0; i < 10; i++) {
+      tone(buzzerPin, 262, 100);
+      delay(150);
+    }
+  }
+
+  codeTaskHandle = NULL;
+  vTaskDelete(NULL); // Self-delete task
+}
+
+void inputManagerTask(void *pvParameters) {
+  int key;
+
+  while (true) {
+    if (xQueueReceive(keypadQueue, &key, portMAX_DELAY)) {
+      if (key == '*') {
+        // Only create a new task if it's not running
+        if (codeTaskHandle == NULL) {
+          xTaskCreatePinnedToCore(getCodeTask, "CodeTask", 4096, NULL, 1, codeTaskHandle, 1);
+        } else {
+          // If task exists, reset input via special key
+          xQueueSend(codeInputQueue, &key, portMAX_DELAY);
+        }
+      }
+      else if (codeTaskHandle != NULL) {
+        // Send other keys to the code task
+        xQueueSend(codeInputQueue, &key, portMAX_DELAY);
+      }
+    }
+  }
+}
+
+
+
+void inactivityTimeoutCallback(TimerHandle_t xTimer) {
+  Serial.println("[Timeout] No activity detected for 10s.");
+
+  if (door_status == DOOR_OPEN) {
+    Serial.println("[Timeout] Closing door...");
+    closeDoor();
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Wait to reach position
+
+    // Use non-blocking delay technique
+    unsigned long closeStart = millis();
+    while (millis() - closeStart < 1000) {
+      vTaskDelay(10 / portTICK_PERIOD_MS); // Let other tasks run
+    }
+  }
+
+  // Send IDLE screen update
+  DisplayMessage msg;
+  msg.state = DISPLAY_IDLE;
+  msg.position = 0;
+
+  if (xQueueSend(displayQueue, &msg, 100) != pdPASS) {
+    Serial.println("[Timeout] Failed to send IDLE message to display queue.");
+  } else {
+    Serial.println("[Timeout] Display set to IDLE.");
+  }
+}
+
+
+// Function to scan keypad and return pressed key
+int checkKeypadStatus() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(KEYPAD_GPIO_OUT[i], HIGH);
+    delay(1);
+    for (int j = 0; j < 4; j++) {
+      if (digitalRead(KEYPAD_GPIO_IN[j]) == HIGH) {
+        delay(KEYPAD_DEBOUNCE_MS);
+        while (digitalRead(KEYPAD_GPIO_IN[j]) == HIGH);  // Wait for release
+        delay(KEYPAD_DEBOUNCE_MS);
+        digitalWrite(KEYPAD_GPIO_OUT[i], LOW);
+        return KEYPAD[j][i];
+      }
+    }
+    digitalWrite(KEYPAD_GPIO_OUT[i], LOW);
+  }
+  return KEYPAD_NOT_PRESSED;
+}
+
+void keypadTask(void *pvParameters) {
+  int key;
+  for(;;) {
+    key = checkKeypadStatus();
+    if (key != KEYPAD_NOT_PRESSED) {
+      xTimerReset(inactivityTimer, 0);
+      xQueueSend(keypadQueue, &key, portMAX_DELAY);
+    }
+    vTaskDelay(50 / portTICK_PERIOD_MS);
+  }
 }
 
 void wifi_init() {
@@ -258,6 +471,34 @@ void wifi_init() {
   Serial.println(WiFi.localIP());
 }
 
+void task_init() {
+  // Tasks
+  xTaskCreate(update_db_file_task, "Update database file", 2048, NULL, 1, db_update_task_h);
+  xTaskCreatePinnedToCore(displayTask, "DisplayTask", 4096, NULL, 1, displayTaskHandle, 1);
+  xTaskCreatePinnedToCore(keypadTask, "KeypadTask", 2048, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(inputManagerTask, "InputManager", 4096, NULL, 1, NULL, 1);
+
+  // Queues
+  displayQueue = xQueueCreate(5, sizeof(DisplayMessage));
+  keypadQueue = xQueueCreate(5, sizeof(int));
+  codeInputQueue = xQueueCreate(5, sizeof(int));
+}
+
+void tft_init() {
+  tft.init();
+  tft.setRotation(3); 
+  backgroundSprite.setColorDepth(8);
+  backgroundSprite.createSprite(240, 135);
+
+  unlockSprite.createSprite(64, 64); // You can pushImage here if you want to pre-fill
+  unlockSprite.setSwapBytes(true);
+  unlockSprite.pushImage(0,0,64,64, unlockIcon);
+
+  lockSprite.createSprite(64, 64); // Same as above
+  unlockSprite.setSwapBytes(true);
+  lockSprite.pushImage(0,0,64,64, lockIcon);
+}
+
 void setup() {
   Serial.begin(115200);
   db_init();
@@ -268,7 +509,9 @@ void setup() {
   }
   Serial.println("Initializing tasks");
   task_init();
-  // port init for servo
+  inactivityTimer = xTimerCreate("InactivityTimer", pdMS_TO_TICKS(10000), pdFALSE, (void *)0, inactivityTimeoutCallback);
+  closeDoor();
+  pinMode(buzzerPin, OUTPUT);
   Serial.println("Initializing WiFi");
   wifi_init();
 
@@ -277,29 +520,20 @@ void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(KEYPAD_GPIO_IN[i], INPUT);
     pinMode(KEYPAD_GPIO_OUT[i], OUTPUT);
+    digitalWrite(KEYPAD_GPIO_OUT[i], LOW);
   }
-  
-  
-
-  tft.init();
-  tft.begin();
-  tft.setRotation(3); 
-  tft.setTextDatum(MC_DATUM);
-  tft.setTextFont(2);
-  tft.setTextColor(TFT_WHITE);
-  tft.setTextSize(2);
-  tft.fillScreen(TFT_BLACK);
-
+    
+  tft_init();
 
   setupApiEndpoints();
-
   server.begin();
+
+  DisplayMessage msg;
+  msg.state = DISPLAY_IDLE;
+  xQueueSend(displayQueue, &msg, portMAX_DELAY);
+  xTimerStart(inactivityTimer, 0);
+
+  Serial.println("Setup complete.");
 }
 
-void loop() {
-  char keyPress = waitForKeyPress();
-  Serial.println(keyPress);
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(50, 50);
-  tft.println(keyPress);
-}
+void loop() {}
